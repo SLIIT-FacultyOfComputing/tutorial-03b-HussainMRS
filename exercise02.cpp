b57e0ac19f769268fa81af81ec7b4d595b21deea
IT21361654 <99306542+HussainMRS@users.noreply.github.com>
2022-02-23 06:41:00 +0000

    completed

--- exercise02.cpp
+++ exercise02.cpp
@@ -6,7 +6,7 @@ int volume(int height, int width, int length);
 int main() {
     int box1Height, box1Width, box1Length;
     int box2Height, box2Width, box2Length;
-    int totalVolume, totalSurface;
+    int totalVolume , v1 , v2 ;
     
     cout << "Enter Box 1 Height : ";
     cin >> box1Height;
@@ -14,20 +14,35 @@ int main() {
     cin >> box1Width;
     cout << "Enter Box 1 Length : ";
     cin >> box1Length;
-    
-     cout << "Enter Box 2 Height : ";
+
+    v1 = volume( box1Height, box1Width, box1Length) ;
+
+    cout<<endl ;
+  
+    cout << "Enter Box 2 Height : ";
     cin >> box2Height;
     cout << "Enter Box 2 Width : ";
     cin >> box2Width;
     cout << "Enter Box 2 Length : ";
     cin >> box2Length;
+
+    cout<<endl ;
+
+    v2 = volume( box2Height, box2Width, box2Length) ;
     
-    totalVolume = volume(box1Height, box1Width, box1Length)
-             + volume(box2Height, box2Width, box2Length);
+    totalVolume = v1 + v2 ;
              
     cout << "Volume of Box is " << totalVolume << endl;
     
     return 0;
 }
 
+int volume(int height, int width, int length)
+{
+  int volume ;
+
+  volume = height * length * width ;
+
+  return volume ;
+}
 // Implement the Volume() function here
--- exercise03.cpp
+++ exercise03.cpp
@@ -6,34 +6,46 @@ int volume(int height, int width, int length);
 
 // 1. Define a structure called Box
 // have the integer data types Height, Width, Length
-
+struct Box
+{
+   int width ;
+   int height ;
+   int length ;
+} ;
 // Do not change the main function
 int main() {
     // 2. Create a variable called box1 of the Box structure type
     // int box1Height, box1Width, box1Length;
-    // 3. Create a variable called box2 of the Box structure type
     
+    // 3. Create a variable called box2 of the Box structure type
+    struct Box box1 , box2 ;
     // int box2Height, box2Width, box2Length;
+     int height, width, length;
+    
     int totalVolume;
     
     // 4. Input the height, width, lenght of box1 and box2
     cout << "Enter Box 1 Height : ";
-    cin >> Height of Box 1;
+    cin >> box1.height;
     cout << "Enter Box 1 Width : ";
-    cin >> Width of Box 1;
+    cin >> box1.width;
     cout << "Enter Box 1 Length : ";
-    cin >> Length of Box 1;
+    cin >> box1.length;
+
+    cout<<"\n" ;
     
-    cout << "Enter Box 2 Height : ";
-    cin >> Height of Box 2;
-    cout << "Enter Box 2 Width : ";
-    cin >> Width of Box 2;
-    cout << "Enter Box 2 Length : ";
-    cin >> Length of Box 2;
+    cout << "Enter Box 1 Height : ";
+    cin >> box2.height;
+    cout << "Enter Box 1 Width : ";
+    cin >> box2.width;
+    cout << "Enter Box 1 Length : ";
+    cin >> box2.length;
+
+    cout<<endl ;
     
     // 5. Replace the coding below to pass the Box type structure
-    totalVolume = volume( , , )
-             + volume( , , );
+    totalVolume = volume( box1.length, box1.width , box1.height )
+             + volume( box2.length, box2.width , box2.height );
     
     cout << "Volume of Box is " << totalVolume << endl;
     
@@ -41,3 +53,11 @@ int main() {
 }
 
 // Implement the functions here
+int volume(int height, int width, int length)
+{
+  int volume ;
+
+  volume = height * length * width ;
+
+  return volume ;
+}
